1f3d29e6c703c4382f8db83d560539d21619cd8c
KingaMendyk <s25275@pjwstk.edu.pl>
2024-11-21 16:06:58 +0100

    Camera position is saved

--- Assets/Scripts/CameraScripts/CameraFollow.cs
+++ Assets/Scripts/CameraScripts/CameraFollow.cs
@@ -4,10 +4,9 @@ namespace CameraScripts {
     public class CameraFollow : MonoBehaviour {
         [SerializeField] private float smoothing = 5f;
         [SerializeField] private Transform target;
-        [SerializeField] private Vector3 offset;
 
         private void Update() {
-            Vector3 newPosition = target.position + offset;
+            Vector3 newPosition = target.position;
             transform.position = Vector3.Lerp (transform.position, newPosition, smoothing * Time.deltaTime);
         }
     }
