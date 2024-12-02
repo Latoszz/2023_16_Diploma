7e08817ca439f48f9d4fcfe63c21e55d3ad62b9e
KingaMendyk <s25275@pjwstk.edu.pl>
2024-12-02 12:33:19 +0100

    Added Tutorial Dialogue for tutorial scene

--- Assets/Scripts/Tutorial/ClickPoint.cs
+++ Assets/Scripts/Tutorial/ClickPoint.cs
@@ -6,6 +6,7 @@ namespace Tutorial {
     public class ClickPoint : MonoBehaviour, IPointerClickHandler {
         public void OnPointerClick(PointerEventData eventData) {
             MouseInputManager.Instance.ForceSetTargetPoint(transform.position);
+            TutorialDialogue.Instance.HideDialogue();
             gameObject.SetActive(false);
         }
     }
---
+++ Assets/Scripts/Tutorial/TutorialDialogue.cs
@@ -0,0 +1,189 @@
+using System.Collections;
+using System.Collections.Generic;
+using Audio;
+using InputScripts;
+using TMPro;
+using UI.Dialogue;
+using UI.HUD;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Tutorial {
+    public class TutorialDialogue: MonoBehaviour, IPointerClickHandler {
+        [Header("References")] 
+        [SerializeField] private GameObject dialoguePanel;
+        [SerializeField] private TMP_Text dialogueText;
+    
+        [SerializeField] private float typingSpeed;
+
+        [Header("Audio")] 
+        [SerializeField] private DialogueAudioConfig defaultAudioConfig;
+        [SerializeField] private bool makePredictable;
+        private AudioSource audioSource;
+
+        [SerializeField] private List<DialogueText> tutorialTexts;
+
+        private const string HTML_ALPHA = "<color=#00000000>";
+
+        private Queue<string> sentences = new Queue<string>();
+        private string sentence;
+        private DialogueText dialogue;
+    
+        private bool wasSkipped;
+        private bool isTyping;
+        private bool conversationEnded;
+        private bool dialogueClosed;
+        public bool DialogueClosed => dialogueClosed;
+
+        public static TutorialDialogue Instance;
+
+        private void Awake() {
+            if (Instance == null) {
+                Instance = this;
+            }
+            else if (Instance != this) {
+                Destroy(gameObject);
+            }
+
+            audioSource = this.gameObject.GetComponent<AudioSource>();
+        }
+
+        private void Start() {
+            DisplaySentence(tutorialTexts[0]);
+        }
+    
+        public void DisplaySentence(DialogueText dialogue) {
+            this.dialogue = dialogue;
+            dialogueClosed = false;
+        
+            if (sentences.Count == 0) {
+                if (!conversationEnded) {
+                    StartConversation(dialogue);
+                }
+                else if (conversationEnded && !isTyping) {
+                    EndConversation();
+                    return;
+                }
+            }
+
+            if (!isTyping) {
+                sentence = sentences.Dequeue();
+                dialogueText.text = sentence;
+                ShowDialogue();
+            }
+            else {
+                ShowAllText();
+            }
+        
+            if (sentences.Count == 0) {
+                conversationEnded = true;
+            }
+        }
+
+        private void StartConversation(DialogueText dialogue) {
+            if (!dialoguePanel.activeSelf) {
+                dialoguePanel.SetActive(true);
+            }
+            SetDialogue(dialogue);
+            HUDController.Instance.HideHUD();
+        }
+
+        private void EndConversation() {
+            conversationEnded = false;
+            sentences.Clear();
+            HideDialogue();
+            HUDController.Instance.ShowHUD();
+        }
+    
+        private void ShowDialogue() {
+            isTyping = false;
+            InputManager.Instance.DisableInput();
+            StopAllCoroutines();
+            StartCoroutine(TypeSentence());
+        }
+    
+        public void HideDialogue() {
+            InputManager.Instance.EnableInput();
+            dialogueClosed = true;
+            dialoguePanel.SetActive(false);
+        }
+    
+        private void SetDialogue(DialogueText dialogue) {
+            foreach (string sentence in dialogue.Sentences) {
+                sentences.Enqueue(sentence);
+            }
+        }
+    
+        private IEnumerator TypeSentence() {
+            isTyping = true;
+            dialogueText.text = "";
+            string originalText = sentence;
+            string displayedText = "";
+            int alphaIndex = 0;
+        
+            foreach (char letter in sentence) {
+                if (dialogueText.text != "") {
+                    PlayDialogueSound(alphaIndex, originalText[alphaIndex]);
+                }
+
+                alphaIndex++;
+                dialogueText.text = originalText;
+                displayedText = dialogueText.text.Insert(alphaIndex, HTML_ALPHA);
+                dialogueText.text = displayedText;
+                yield return new WaitForSeconds(1/typingSpeed);
+            }
+            audioSource.Stop();
+            isTyping = false;
+        }
+
+        private void ShowAllText() {
+            StopAllCoroutines();
+            dialogueText.text = sentence;
+            audioSource.Stop();
+            isTyping = false;
+        }
+        
+        public void OnPointerClick(PointerEventData eventData) {
+            if(dialoguePanel.activeSelf)
+                DisplaySentence(dialogue);
+        }
+
+        private void PlayDialogueSound(int currentDisplayedCharacterCount, char currentCharacter) {
+            AudioClip[] dialogueAudios = defaultAudioConfig.dialogueAudios;
+            int frequencyLevel = defaultAudioConfig.frequencyLevel;
+            float minPitch = defaultAudioConfig.minPitch;
+            float maxPitch = defaultAudioConfig.maxPitch;
+            bool stopAudioSource = defaultAudioConfig.stopAudioSource;
+        
+            if (currentDisplayedCharacterCount % frequencyLevel == 0) {
+                if(stopAudioSource)
+                    audioSource.Stop();
+
+                AudioClip audioClip = null;
+                if (makePredictable) {
+                    int hashCode = (int)currentCharacter + 10000;
+                    int index = hashCode % dialogueAudios.Length;
+                    audioClip = dialogueAudios[index];
+
+                    int minPitchInt = (int)(minPitch * 100);
+                    int maxPitchInt = (int)(maxPitch * 100);
+                    int pitchRangeInt = maxPitchInt - minPitchInt;
+                    if (pitchRangeInt != 0) {
+                        int predictablePitchInt = (hashCode % pitchRangeInt) + minPitchInt;
+                        float predictablePitch = predictablePitchInt / 100f;
+                        audioSource.pitch = predictablePitch;
+                    }
+                    else {
+                        audioSource.pitch = minPitch;
+                    }
+                }
+                else {
+                    int audioIndex = Random.Range(0, dialogueAudios.Length);
+                    audioClip = dialogueAudios[audioIndex];
+                    audioSource.pitch = Random.Range(minPitch, maxPitch);
+                }
+                audioSource.PlayOneShot(audioClip);
+            }
+        }
+    }
+}
