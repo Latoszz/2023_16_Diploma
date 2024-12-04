03ed3a970184d110505a2550891a462938b62b45
KingaMendyk <s25275@pjwstk.edu.pl>
2024-12-04 19:50:23 +0100

    Camera shakes near statue

--- Assets/Scripts/Interaction/Objects/Statue.cs
+++ Assets/Scripts/Interaction/Objects/Statue.cs
@@ -1,14 +1,24 @@
+using System;
+using System.Collections;
+using CameraScripts;
+using Effects;
 using Esper.ESave;
 using Events;
+using InputScripts;
 using Interaction.Scene;
 using SaveSystem;
+using Tutorial;
+using UI.Inventory;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace Interaction.Objects {
     public class Statue : MonoBehaviour, IPointerClickHandler, ISavable {
         [SerializeField] private ShowOutline showOutlineScript;
+        [SerializeField] private GameObject shakeCamera;
         [SerializeField] private string sceneName;
+
+        private bool isActive;
 
         private const string StatueSaveID = "StatueSave";
         //private bool entered = false;
@@ -22,21 +32,15 @@ namespace Interaction.Objects {
 
         private void Activate() {
             showOutlineScript.enabled = true;
+            shakeCamera.SetActive(true);
+            isActive = true;
         }
 
-        private void Start() {
-            /*
-            if (entered) {
-                GetComponent<ShowOutline>().enabled = false;
-            }
-            */
-        }
-        
         public void OnPointerClick(PointerEventData eventData) {
             //if (entered) return;
             //entered = true;
             //SaveManager.Instance.SaveGame();
-            if (showOutlineScript.enabled == false)
+            if (!isActive || shakeCamera.activeSelf)
                 return;
             SceneSwitcher.Instance.LoadScene(sceneName);
         }
