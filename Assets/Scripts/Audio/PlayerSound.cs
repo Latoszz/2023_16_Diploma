2651ebb6c8f6dcdc6cf0e0da45f53d0b10090077
KingaMendyk <s25275@pjwstk.edu.pl>
2024-12-16 10:46:39 +0100

    Update PlayerSound.cs

--- Assets/Scripts/Audio/PlayerSound.cs
+++ Assets/Scripts/Audio/PlayerSound.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 
 namespace Audio {
      public class PlayerSound : MonoBehaviour {
+          [SerializeField] private float minVolume = 0.1f;
+          [SerializeField] private float maxVolume = 0.3f;
+          [SerializeField] private float minPitch = 0.8f;
+          [SerializeField] private float maxPitch = 1.2f;
+          
           public List<AudioClip> footstepSounds;
           private AudioSource audioSource;
 
@@ -13,8 +18,8 @@ namespace Audio {
           public void PlayFootstep() {
                AudioClip clip = footstepSounds[Random.Range(0, footstepSounds.Count)];
                audioSource.clip = clip;
-               audioSource.volume = Random.Range(0.1f, 0.3f);
-               audioSource.pitch = Random.Range(0.8f, 1.2f);
+               audioSource.volume = Random.Range(minVolume, maxVolume);
+               audioSource.pitch = Random.Range(minPitch, maxPitch);
                audioSource.Play();
           }
      }
