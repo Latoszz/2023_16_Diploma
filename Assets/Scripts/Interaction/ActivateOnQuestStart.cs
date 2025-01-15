79776d41deecad3f4c0d3006dbbebd3c196855b3
KingaMendyk <s25275@pjwstk.edu.pl>
2025-01-15 20:51:19 +0100

    Added Deafeat Kiryl quest

---
+++ Assets/Scripts/Interaction/ActivateOnQuestStart.cs
@@ -0,0 +1,41 @@
+using Esper.ESave;
+using Events;
+using QuestSystem;
+using SaveSystem;
+using UnityEngine;
+
+namespace Interaction {
+    public class ActivateOnQuestStart : MonoBehaviour, ISavable {
+        [SerializeField] private QuestInfoSO questInfoSo;
+        [SerializeField] private GameObject objectToActivate;
+        private bool activated;
+        
+        private void OnEnable() {
+            GameEventsManager.Instance.QuestEvents.OnStartQuest += Activate;
+        }
+
+        private void OnDisable() {
+            GameEventsManager.Instance.QuestEvents.OnStartQuest -= Activate;
+        }
+
+        private void Start() {
+            objectToActivate.SetActive(activated);
+        }
+
+        private void Activate(string questId) {
+            if (questId == questInfoSo.id) {
+                objectToActivate.SetActive(true);
+                activated = true;
+            }
+        }
+        
+        public void PopulateSaveData(SaveFile saveFile) {
+            saveFile.AddOrUpdateData("DeactivatedObject_" + objectToActivate.name, activated);
+        }
+
+        public void LoadSaveData(SaveFile saveFile) {
+            if(saveFile.HasData("DeactivatedObject_" + objectToActivate.name))
+                activated = saveFile.GetData<bool>("DeactivatedObject_" + objectToActivate.name);
+        }
+    }
+}
