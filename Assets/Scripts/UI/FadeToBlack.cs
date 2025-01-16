96a7f67f5105885791b0acd44972c7cb2ba33612
KingaMendyk <s25275@pjwstk.edu.pl>
2025-01-16 15:46:22 +0100

    Added fade to black effect

---
+++ Assets/Scripts/UI/FadeToBlack.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using DG.Tweening;
+using Events;
+using QuestSystem;
+using UI.Dialogue;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+    public class FadeToBlack : MonoBehaviour {
+        [SerializeField] private QuestInfoSO questInfoSo;
+        [SerializeField] private Image fadeImage;
+        [Range(0, 5)] [SerializeField] private float duration = 1f;
+        
+        
+        private void OnEnable() {
+            GameEventsManager.Instance.QuestEvents.OnStartQuest += FadeScreen;
+        }
+
+        private void OnDisable() {
+            GameEventsManager.Instance.QuestEvents.OnStartQuest -= FadeScreen;
+        }
+
+        private void Start() {
+            fadeImage.color = new Color(0, 0, 0, 0);
+        }
+
+        private void FadeScreen(string questId) {
+            if (questId == questInfoSo.id) {
+                StartCoroutine(FadeCoroutine());
+            }
+        }
+
+        private IEnumerator FadeCoroutine() {
+            yield return new WaitUntil(() => DialogueController.Instance.DialogueClosed);
+            Fade();
+        }
+        
+        private void Fade() {
+            fadeImage.DOFade(1f, duration).OnComplete(() =>
+            {
+                fadeImage.DOFade(0f, duration);
+            });
+        }
+    }
+}
